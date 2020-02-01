c5fe56db9993adf3614b5db52ab0a04590164fd1
Bryann Sotomayor-Rinaldi <bryann.sotomayor@gmail.com>
2020-02-01 14:27:13 -0800

    Fetches photos from API

--- src/contexts/Context.js
+++ src/contexts/Context.js
@@ -1,8 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const { Provider, Consumer } = React.createContext();
 const ContextProvider = (props) => {
 	const [photos, setPhotos] = useState([]);
+
+	useEffect(() => {
+		fetch('https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json')
+			.then(response => response.json())
+			.then(data => setPhotos(data));
+	}, []);
+	console.log(photos);
 	return (
 		<Provider value={{ photos }}>{props.children}</Provider>
 	);
