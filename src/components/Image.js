61ba9d92eba5ec2934b3eab1893df0ca88ebe6b5
Bryann Sotomayor-Rinaldi <bryann.sotomayor@gmail.com>
2020-02-02 10:41:28 -0800

    Displays a filled heart icon when the photo is favorited.

--- src/components/Image.js
+++ src/components/Image.js
@@ -4,13 +4,30 @@ import { PhotosContextConsumer } from '../contexts/PhotosContext';
 const Image = ({ className, img }) => {
 	const [hovered, setHovered] = useState(false);
 
-	const heartIcon = hovered &&
-		<PhotosContextConsumer>
-			{({ toggleFavorite }) =>
-				<i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}></i>
-			}
 
+	/*
+				favorite 								show filled heart
+				!favorite && hovered		show empty heart
+				!favorite && !hovered		don't show anything
+	*/
+	// const heartIcon = null;
+	// if (img.isFavorite) heartIcon = 
+	// <i className={`ri-heart-fill favorite`} onClick={() => toggleFavorite(img.id)}></i>
+	// else if (hovered) <i className={`ri-heart-line favorite`} onClick={() => toggleFavorite(img.id)}></i>
+
+
+	const heartIcon =
+		<PhotosContextConsumer>{
+			({ toggleFavorite }) =>
+				<i
+					className={`favorite ${
+						img.isFavorite ? 'ri-heart-fill' :
+							hovered ? 'ri-heart-line' : null}`
+					}
+					onClick={() => toggleFavorite(img.id)}></i>
+		}
 		</PhotosContextConsumer>
+
 
 	const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>;
 
