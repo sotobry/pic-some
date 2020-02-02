e1134e6d353563681e526ce9405c11e6eba79939
Bryann Sotomayor-Rinaldi <bryann.sotomayor@gmail.com>
2020-02-01 17:46:43 -0800

    Displays icons on hover.

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,10 +3,16 @@ import React, { useState } from 'react';
 const Image = ({ className, img }) => {
 	const [hovered, setHovered] = useState(false);
 	console.log(img.id, { hovered });
+
+	const heartIcon = hovered && <i className="ri-heart-line favorite"></i>;
+	const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>;
+
 	return (
 		<div className={`${className} image-container ${hovered ? 'hovered' : null}`}
 			onMouseEnter={() => setHovered(true)}
 			onMouseLeave={() => setHovered(false)} >
+			{heartIcon}
+			{cartIcon}
 			<img className='image-grid' src={img.url} alt='' />
 		</div>
 	);
