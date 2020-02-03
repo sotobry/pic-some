5577b03e914bd610ddbb7e93bac7a4406690b12b
Bryann Sotomayor-Rinaldi <bryann.sotomayor@gmail.com>
2020-02-03 10:06:30 -0800

    Calculates the total cost of the items added to the cart.

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -8,11 +8,12 @@ const Cart = () =>
 			const cartItemElements = cartItems.map(item =>
 				<CartItem key={item.id} item={item} />);
 
+			const total = (cartItems.length * 4.99).toLocaleString("en-US", { style: "currency", currency: "USD" });
 			return (
 				<main className='cart-page'>
 					<h1>Check out</h1>
 					{cartItemElements}
-					<p className='total-cost'>Total: </p>
+					<p className='total-cost'>Total: {total}</p>
 					<div className='order-button'>
 						<button>Place Order</button>
 					</div>
