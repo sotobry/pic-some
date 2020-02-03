a93e7a883f9370ce6a6f15fb82b009485806e509
Bryann Sotomayor-Rinaldi <bryann.sotomayor@gmail.com>
2020-02-03 09:56:04 -0800

    Adds functionality so that the user can remove items from the cart page.

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,9 +1,13 @@
 import React from 'react';
-// import { PhotosContextConsumer } from '../contexts/PhotosContext';
+import { PhotosContextConsumer } from '../contexts/PhotosContext';
 
 const CartItem = ({ item }) =>
 	<div className='cart-item'>
-		<i className='ri-delete-bin-line'></i>
+		<PhotosContextConsumer>{
+			({ removeImgFromCart }) =>
+				<i className='ri-delete-bin-line' onClick={() => removeImgFromCart(item)}></i>
+		}
+		</PhotosContextConsumer>
 		<img src={item.url} width='130px' alt='' />
 		<p>$4.99</p>
 	</div>
