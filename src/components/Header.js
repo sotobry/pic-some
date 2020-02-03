a7e91f3e9e6de8cf14ad5f8619aceb32307442a6
Bryann Sotomayor-Rinaldi <bryann.sotomayor@gmail.com>
2020-02-03 09:05:01 -0800

    Updates header cart icon so that it shows full if there's any items in the cart.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-const Header = () => {
-	return (
-		<header>
-			<Link to='/'><h1>Pic Some</h1></Link>
-			<Link to='/cart'><i className='ri-shopping-cart-line ri-fw ri-2x'></i></Link>
-		</header>
-	);
-};
+import { PhotosContextConsumer } from '../contexts/PhotosContext';
+
+
+const Header = () =>
+	<PhotosContextConsumer>{
+		({ cartItems }) =>
+			<header>
+				<Link to='/'><h1>Pic Some</h1></Link>
+				<Link to='/cart'>
+					<i className={`ri-fw ri-2x 
+					ri-shopping-cart-${cartItems.length ?
+							'fill' :
+							'line'}`
+					}></i>
+				</Link>
+			</header >
+	}
+	</PhotosContextConsumer>
 
 export { Header };
